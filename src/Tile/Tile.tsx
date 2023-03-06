8b808d495f4c9f4bacd33bfff11fb7ed9446e695
Rafał Karczmarzyk <karczmarzyk.rafal@gmail.com>
2023-03-06 19:20:00 +0100

    add svgr transform

--- src/Tile/Tile.tsx
+++ src/Tile/Tile.tsx
@@ -1,18 +1,19 @@
 import React, { FC } from 'react';
 
+import SvgBambooOne from '../Icons/BambooOne';
 import { TOrientation } from '../types';
-import icons, { TIcons } from './icons';
+//import icons, { TIcons } from './icons';
 import * as S from './Tile.styled';
 
 interface Props {
-  kind: keyof TIcons;
+  kind: any;
   orientation?: TOrientation;
 }
 
 const Tile: FC<Props> = ({ kind, orientation = 'bottom' }) => {
   return (
     <S.Wrapper orientation={orientation}>
-      <S.Vector src={icons[kind]} />
+        <SvgBambooOne width={50} height={50} />
     </S.Wrapper>
   );
 };
